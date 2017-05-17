675bf9aa4d15b2520abc221e9882a00c62ef10c6
Charlie Fisher <charliefisher@crescentschool.org>
2017-05-17 09:18:36 -0400

    Finished buttons classes.

--- src/main/ProductButton.java
+++ src/main/ProductButton.java
@@ -4,22 +4,10 @@ public class ProductButton extends Button {
 
 	private double price;
 	private static final int size = 150;
-	private int xCord, yCord;
 
 	public ProductButton(String name, int x, int y, double price){
-		super(name);
-		
-		this.xCord = x;
-		this.yCord = y;	
+		super(name, x, y, size, size);
 		this.price = price;
-	}
-	
-	@Override
-	public boolean clicked(int x, int y) {
-		if ((x > xCord && x < xCord + size) && (y > yCord && y < yCord + size)) {
-			return true;
-		}
-		return false;
 	}
 	
 	public double getPrice(){
