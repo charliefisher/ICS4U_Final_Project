2470438fedd3ab918dd31c6fd6cd088465e1dd50
Charlie Fisher <charliefisherhome@gmail.com>
2017-05-21 01:05:41 -0400

    Fixed creating a transaction and a new customer bug

--- src/main/CashMachine.java
+++ src/main/CashMachine.java
@@ -336,13 +336,14 @@ public class CashMachine {
 	}
 	
 	private void loadCustomer() throws IOException {
+		this.transaction = new Transaction(this.customerName);
 		if(this.customer.load(this.customerName) && this.customerNumber.length()-1 == 10) {
 			this.state = State.ORDER;
-			this.transaction = new Transaction(this.customerName);
 		}
-		else if(this.customerNumber.length()-1 == 10) {					
-			this.state = State.ORDER;
+		else if(this.customerNumber.length()-1 == 10) {	
+			this.transaction = new Transaction(this.customerName);
 			this.customer.create(this.customerName, this.customerNumber);
+			this.state = State.ORDER;
 		}
 	}
 }
