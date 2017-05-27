0f35b3f02b934f98b98ddc4bb810332d5c100496
Charlie Fisher <charliefisherhome@gmail.com>
2017-05-27 13:34:54 -0400

    Commented database element

--- src/database/DatabaseElement.java
+++ src/database/DatabaseElement.java
@@ -3,15 +3,17 @@ package database;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+// an abstract class that is extended by all database elements in this application (customer, transactions)
 public abstract class DatabaseElement {
 
+	// default constructor
 	public DatabaseElement() {
-
 	}
 
+	// tries to load a file and returns a boolean based on whether or not the file exists
 	public abstract boolean load(String fileName);
-
+	// instantiates instance variables from reading the database file
 	public abstract void getInfo() throws FileNotFoundException;
-
+	// writes all instance variables to the file (updates changes in the database)
 	public abstract void write() throws IOException;
 }
